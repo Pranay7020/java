f55bf6e36b2b2f9d0ee677851a568aac459798a8
Pranay7020 <pranayshinde872@gmail.com>
2025-07-27 23:01:44 +0530

    added numbercheck.java

---
+++ NumberCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt(); // Taking user input
+
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else if (number < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+
+        scanner.close(); // Close the scanner
+    }
+}
