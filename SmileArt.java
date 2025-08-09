2eba40086e70799c35757fe398239cf4e5c90fca
Pranay7020 <pranayshinde872@gmail.com>
2025-08-09 22:59:08 +0530

    Create SmileArt.java

---
+++ SmileArt.java
@@ -0,0 +1,11 @@
+public class SmileArt {
+    public static void main(String[] args) {
+        System.out.println("   *****   ");
+        System.out.println(" *       * ");
+        System.out.println("*  O   O  *");
+        System.out.println("*    ^    *");
+        System.out.println("*  \\___/  *");
+        System.out.println(" *       * ");
+        System.out.println("   *****   ");
+    }
+}
