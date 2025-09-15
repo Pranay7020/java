5a5fa20423645fbde218057a4b6c74d938656c82
Pranay7020 <pranayshinde872@gmail.com>
2025-09-15 21:40:23 +0530

    added new

--- javastart/boilerplate.java
+++ javastart/boilerplate.java
@@ -1,6 +1,5 @@
 public class boilerplate {
     public static void main(String[] args) {
-        System.out.println("pranay");
-        System.out.println("pranay");
+        
     }
 }
---
+++ javastart/outputinj.java
@@ -0,0 +1,7 @@
+public class outputinj  {
+    public static void main(String[] args) {
+        System.out.println("pranay");
+        System.out.println("pranay");
+    }       
+    
+}
