7d275d349bee6b6af22dd0d24e2f6da6db221019
Pranay7020 <pranayshinde872@gmail.com>
2025-09-21 22:55:56 +0530

    added new

--- javastart/condistional_stmt/ternary_operator.java
+++ javastart/condistional_stmt/ternary_operator.java
@@ -2,7 +2,7 @@ package condistional_stmt;
 import java.util.*;
 public class ternary_operator {
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+        Scanner sc =new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
     
---
+++ javastart/loops/type_n_number.java
@@ -0,0 +1,14 @@
+package loops;
+import java.util.*;
+
+public class type_n_number {
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int i=1;
+    while(i<=n){
+        System.out.println(i);
+        i++;
+    }                               
+}
+}
